af7adf0fedc110829e2a7b77f2db2a8b947d28f1
Kartik Maandothiya <kartikya27@gmail.com>
2025-06-06 19:10:45 +0530

    kayout

--- src/shop/services/orderService.js
+++ src/shop/services/orderService.js
@@ -1,5 +1,5 @@
 import BaseService from './baseService.js';
-import { Order } from '../models/orders/index.js';
+import { Order } from '../../models/orders/order.js';
 
 class OrderService extends BaseService {
 
